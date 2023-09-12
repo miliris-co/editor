49237eb05c0f59e713285e9a2192cef8133e64ce
Igor Michalak <git@igormichalak.com>
2023-09-12 16:20:27 +0200

    feat: add styling configuration

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,7 @@
-import './style.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+
+import '@unocss/reset/tailwind.css';
+import './global.css';
 import 'virtual:uno.css';
--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,24 @@
-import { defineConfig } from 'unocss';
+import {
+    defineConfig,
+    presetWind,
+    presetAttributify,
+    presetWebFonts,
+} from 'unocss';
+import transformerVariantGroup from '@unocss/transformer-variant-group';
 
-export default defineConfig({});
+export default defineConfig({
+    presets: [
+        presetWind(),
+        presetAttributify(),
+        presetWebFonts({
+            provider: 'none',
+            fonts: {
+                inter: [
+                    { name: 'Inter', weights: [400, 600, 700] },
+                    { name: 'sans-serif' },
+                ],
+            },
+        }),
+    ],
+    transformers: [transformerVariantGroup()],
+});
